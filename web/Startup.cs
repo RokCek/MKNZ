83caaa5abd15647a15d3219124bd1bcedf1598a2
Rok Cek <rok.cek@gmail.com>
2021-01-18 05:24:44 +0100

    povezava z azure

--- web/Startup.cs
+++ web/Startup.cs
@@ -37,8 +37,14 @@ namespace web
            // services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
             //.AddEntityFrameworkStores<MKNZContext>();
 
+            //services.AddDbContext<MKNZContext>(options =>
+            //options.UseSqlServer(Configuration.GetConnectionString("MKNZContext")));
+
+            
             services.AddDbContext<MKNZContext>(options =>
-            options.UseSqlServer(Configuration.GetConnectionString("MKNZContext")));
+            options.UseSqlServer(Configuration.GetConnectionString("AzureContext")));
+
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
